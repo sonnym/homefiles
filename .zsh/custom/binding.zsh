1e6ae308dccd504d3b75de49b4e5f483d3c4422f
sonnym <michaud.sonny@gmail.com>
2014-12-23 14:59:48 -0500

    restore shift tab completion menu binding

--- .zsh/custom/binding.zsh
+++ .zsh/custom/binding.zsh
@@ -1,3 +1,6 @@
 autoload -U edit-command-line
 zle -N edit-command-line
 bindkey '\C-x\C-e' edit-command-line
+
+# [SHIFT-TAB] - move through completion menu backwards
+bindkey "${terminfo[kcbt]}" reverse-menu-complete
