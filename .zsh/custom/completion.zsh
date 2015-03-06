0d2fd12ca6d1859450280ef4e47e933d8ae7a89e
sonnym <michaud.sonny@gmail.com>
2015-03-06 12:12:37 -0500

    further simplify completion

--- .zsh/custom/completion.zsh
+++ .zsh/custom/completion.zsh
@@ -2,17 +2,6 @@ zmodload -i zsh/complist
 
 zstyle ':completion:*' list-colors ''
 
-# should this be in keybindings?
-bindkey -M menuselect '^o' accept-and-infer-next-history
-
 zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
 zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
-
-# disable named-directories autocompletion
-zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
-cdpath=(.)
-
-# Use caching so that commands like apt and dpkg complete are useable
-zstyle ':completion::complete:*' use-cache 1
-zstyle ':completion::complete:*' cache-path $ZSH/cache/
