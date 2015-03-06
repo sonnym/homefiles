657cff939f3e786604c626d3f0874868c6967068
sonnym <michaud.sonny@gmail.com>
2015-03-06 12:28:55 -0500

    enable vi mode preserving history substring search

--- .zsh/custom/binding.zsh
+++ .zsh/custom/binding.zsh
@@ -1,6 +1,12 @@
+bindkey -v
+
 autoload -U edit-command-line
 zle -N edit-command-line
 bindkey '\C-x\C-e' edit-command-line
+
+# [CTRL-r] - history search
+bindkey '\C-r' history-incremental-search-backward
+bindkey '\C-s' history-incremental-search-forward
 
 # [SHIFT-TAB] - move through completion menu backwards
 bindkey "${terminfo[kcbt]}" reverse-menu-complete
