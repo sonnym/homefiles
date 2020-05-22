fee0818e4f846f4037fbcaf50fe1f89df41d1d21
Sonny Michaud <michaud.sonny@gmail.com>
2020-05-22 12:45:53 -0500

    prefer npx

--- .zsh/custom/aliases.zsh
+++ .zsh/custom/aliases.zsh
@@ -3,13 +3,6 @@ alias python='python3'
 alias sev='sudo -E etckeeper vcs'
 alias xclip='xclip -i -selection clipboard'
 
-function npm-exec() {
-  PATH=$(npm bin):$PATH $@
-}
-function yarn-exec() {
-  PATH=$(yarn bin):$PATH $@
-}
-
 function node-inspect() {
   node inspect $(which $1) ${@: (2,-1)}
 }
